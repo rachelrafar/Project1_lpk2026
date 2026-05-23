bcf25ff7d5300a4c06a85c602576c8222edba7da
rachelrafar <rachelrafar@gmail.com>
2026-05-23 12:19:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -863,87 +863,6 @@ elif menu=="📚 Informasi Bahan Kimia":
 </div>
 """, unsafe_allow_html=True)
 
-# ================= ANALISIS KIMIA =================
-
-elif menu=="🧪 Analisis Kimia":
-
-    st.title("🧪 Smart Chemical Analysis")
-
-    if st.button("⬅ Kembali ke Home"):
-        go_to("🏠 Home")
-
-    senyawa=st.selectbox(
-    "Pilih Senyawa",
-    list(db.keys())
-    )
-
-    data=db[senyawa]
-
-    st.markdown(f"""
-    <div class='info-box'>
-    
-    <h3>📊 Hasil Analisis Senyawa</h3>
-
-    <b>🧪 Nama :</b> {data[0]} <br><br>
-
-    <b>📌 Rumus :</b> {senyawa} <br><br>
-
-    <b>⚗️ Jenis :</b> {data[1]} <br><br>
-
-    <b>⚖️ Mr :</b> {data[2]} <br><br>
-
-    <b>⚠️  Bahaya :</b> {data[3]} <br><br>
-
-    <b>🧬 Struktur :</b> {data[5]}
-
-    </div>
-    """, unsafe_allow_html=True)
-
-    st.subheader("📈 Interpretasi Kimia")
-
-    if "Asam" in data[1]:
-
-        st.success("""
-Senyawa ini bersifat asam dan menghasilkan ion H+ dalam larutan.
-Digunakan pada analisis laboratorium dan industri kimia.
-""")
-
-    elif "Basa" in data[1]:
-
-        st.info("""
-Senyawa ini bersifat basa dan menghasilkan ion OH- dalam larutan.
-Umumnya digunakan untuk netralisasi dan industri.
-""")
-
-    elif "Garam" in data[1]:
-
-        st.warning("""
-Senyawa ini termasuk golongan garam hasil reaksi asam dan basa.
-""")
-
-    else:
-
-        st.write("""
-Senyawa ini memiliki karakteristik kimia khusus berdasarkan gugus fungsinya.
-""")
-
-    fakta=random.choice([
-
-    "Larutan asam kuat terionisasi sempurna di dalam air.",
-
-    "NaOH merupakan salah satu basa kuat paling umum di laboratorium.",
-
-    "H2SO4 digunakan pada baterai kendaraan.",
-
-    "Etanol digunakan sebagai antiseptik.",
-
-    "pH menentukan tingkat keasaman larutan."
-
-    ])
-
-    st.info(f"🧠 Fakta Kimia : {fakta}")
-
-
 # ================= TENTANG =================
 
 elif menu=="ℹ️ Tentang":
@@ -963,7 +882,7 @@ elif menu=="ℹ️ Tentang":
     <li>Smart Solution Maker</li>
     <li>Smart pH Calculator</li>
     <li>Informasi Bahan Kimia</li>
-    <li>Smart Chemical Analysis</li>
+    
     </ul>
 
     <h4>👨‍💻 Teknologi</h4>
